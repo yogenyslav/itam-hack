ff81d7868a946cdb729713789bf67e97ab544f63
Evgeny Gurov <evgenygurov9@mail.ru>
2023-09-09 13:36:58 +0300

    internal_role hotfix

--- src/user/domain.py
+++ src/user/domain.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from enum import Enum
 from pydantic import BaseModel, Field, ConfigDict
 
@@ -16,7 +17,7 @@ class UserDto(BaseModel):
     )
     first_name: str = Field(..., min_length=1, max_length=50, example="Михаил")
     last_name: str = Field(..., min_length=1, max_length=50, example="Сурначев")
-    internal_role: UserInternalRole = Field(
+    internal_role: Optional[UserInternalRole] = Field(
         ..., min_length=1, max_length=50, example="student"
     )
     level: float = Field(..., ge=0, example=4.7)
